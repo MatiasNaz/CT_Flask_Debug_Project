e2604f48d249feb028ec035a74863cc8eedf796e
Matias Nazabal <mgnazabal@gmail.com>
2022-01-22 22:52:36 -0500

    hw commit

--- debug_project_app/routes.py
+++ debug_project_app/routes.py
@@ -1,5 +1,6 @@
-from debug_project_app import app, Message, mail
+from debug_project_app import app, Message, mail, db
 from flask import render_template, request, redirect, url_for
+
 
 # Import for Forms
 from debug_project_app.forms import UserInfoForm, PostForm, LoginForm
@@ -10,17 +11,18 @@ from debug_project_app.models import User, Post, check_password_hash
 # Import for Flask Login - login_required, login_user,current_user, logout_user
 from flask_login import login_required,login_user, current_user,logout_user
 
+
 # Home Route
-@app.route('/')
+@app.route('/', methods=['GET'])
 def home():
-    posts = Post.query.all
-    returnrender_template("homes.html", posts = posts)
+    posts = Post.query.all()
+    return render_template("home.html", posts = posts)
 
 # Register Route
-@app.route('/register', methods=['GET','POST'])
+@app.route('/register', methods=['GET'])
 def register():
     form = UserInfoForm()
-    if request.method = 'POST' and form.validate():
+    if request.method == 'POST' and form.validate():
         # Get Information
         username = form.username.data
         password = form.password.data
